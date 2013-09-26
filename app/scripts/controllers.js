24b19fa134b8d78220237cfbb3c62d14f20afc4f
ygayl <yassine.gayl@gmail.com>
2013-09-26 13:04:21 +0200

    no more button modif

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -24,26 +24,8 @@ meetup.controller('TweetListCtrl', function ($scope, $location) {
 
     $scope.numLimit = 5;
 
-    function updateStatusBar() {
-
-        if (!$scope.tweets.$$v || $scope.tweets.$$v.hits.total == 0) {
-            $scope.hasMore = false;
-
-        } else if ($scope.tweets.$$v.hits.total > $scope.numLimit) {
-            $scope.hasMore = true;
-        }
-    }
-
-
-    $scope.$watch('tweets', function () {
-        console.log('New tweet,list updated!');
-        console.log($scope.tweets);
-        updateStatusBar();
-    });
-
     $scope.show_more = function () {
         $scope.numLimit += 5;
-        updateStatusBar();
     }
 
     $scope.goConsultTweet = function (tweet) {
